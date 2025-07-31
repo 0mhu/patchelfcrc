3ff626134e2f5ec8cb5a5feae6e32645442575ef
Mario <mario.huettel@linux.com>
2025-07-31 20:28:18 +0200

    Allow build without git

--- gen_version_header.sh
+++ gen_version_header.sh
@@ -4,7 +4,12 @@ if [[ -z $1 ]]; then
 	exit -1;
 fi
 
-ver=`git describe --tags --always --dirty`
+if [[ "$2" == "dummy" ]]; then
+	ver="v0.0.0-undefined"
+else
+	ver=`git describe --tags --always --dirty`
+fi
+
 echo "#ifndef _VERSION_GENERATED_H_" > $1
 echo "#define _VERSION_GENERATED_H_" >> $1
 echo "#define GIT_VERSION_STRING \"$ver\"" >> $1
